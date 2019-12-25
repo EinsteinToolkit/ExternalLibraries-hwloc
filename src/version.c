57c675e4ac075eb7b0e1bebb48ecee174b2e5f5e
Erik Schnetter <schnetter@gmail.com>
2019-12-25 11:40:51 -0500

    Output library version as well

--- src/version.c
+++ src/version.c
@@ -3,11 +3,15 @@
 #include <cctk.h>
 
 int hwloc_version(void) {
+  const char *library_version = HWLOC_VERSION;
   unsigned buildtime_api_version = HWLOC_API_VERSION;
   unsigned runtime_api_version = hwloc_get_api_version();
+  // TODO: Check only major version number?
   if (runtime_api_version != buildtime_api_version)
-    CCTK_VERROR("build-time API version: 0x%x, run-time API version: 0x%x",
-                buildtime_api_version, runtime_api_version);
-  CCTK_VINFO("API version 0x%x", buildtime_api_version);
+    CCTK_VERROR("library version %s, build-time API version 0x%x, run-time API "
+                "version 0x%x",
+                library_version, buildtime_api_version, runtime_api_version);
+  CCTK_VINFO("library version %s, API version 0x%x", library_version,
+             buildtime_api_version);
   return 0;
 }
